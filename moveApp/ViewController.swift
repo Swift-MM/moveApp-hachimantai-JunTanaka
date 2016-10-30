d5b73f518fb0c6e14a10be3434fb69d991bd51c8
Jun88368836 <jun88368836@gmail.com>
2016-10-30 16:09:33 +0900

    Initial Commit

---
+++ moveApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moveApp
+//
+//  Created by 田中潤 on 2016/10/30.
+//  Copyright © 2016年 田中潤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
